238c9c47fd40a85d1b841de07f2a7a848ee6a907
Jong <kau96kim@gmail.com>
2020-07-12 02:05:44 +0900

    3Sum
    3Sum

--- threeSum.py
+++ threeSum.py
@@ -1,18 +1,30 @@
 class Solution:
   def threeSum(self, nums: [int]) -> [[int]]:
-    answer = []
+    # O(n^3) version 1
+    # answer = set()
+    # for i, first_value in enumerate(nums):
+    #   for j, second_value in enumerate(nums[i+1:]):
+    #     for third_value in nums[i+j+2:]:
+    #       if first_value + second_value + third_value == 0:
+    #         element = sorted([first_value, second_value, third_value])
+    #         answer.add(tuple(element))
 
+    answer = set()
     nums.sort()
-    negative = [x for x in nums if x < 0]
-    positive = [x for x in nums if x > 0]
-    zero = True if 0 in nums else False
+    length = len(nums)
+    for i, i_value in enumerate(nums):
+      j, k = i + 1, length - 1
 
-    if zero:
-      for x in negative:
-        if abs(x) in positive:
-          answer.append([0, x, abs(x)])
-          print(answer)
-
-    print(answer)
-
-Solution.threeSum(Solution, [-2, 0, 1, 2])
+      while j < k:
+        sum = nums[j] + nums[k] + i_value
+        if sum == 0:
+          element = sorted([i_value, nums[j], nums[k]])
+          answer.add(tuple(element))
+          j += 1
+          k -= 1
+        elif sum < 0:
+          j += 1
+        elif sum > 0:
+          k -= 1
+        
+    return answer
