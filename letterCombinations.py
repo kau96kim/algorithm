98860bc45b1d6963144b0c99958e829f445a987b
Jong <kau96kim@gmail.com>
2020-12-27 13:45:11 +0900

    Letter Combinations of a Phone Number
    leetcode.com/problems/letter-combinations-of-a-phone-number

---
+++ letterCombinations.py
@@ -0,0 +1,28 @@
+class Solution:
+    def recursion(self, index, answer, word, mapping, digits) -> [str]:
+        if index + 1 == len(digits):
+            for i in mapping[int(digits[index])]:
+                answer.append(word + i)
+            return answer
+        else:
+            for i in mapping[int(digits[index])]:
+                self.recursion(self, index+1, answer, word + i, mapping, digits)
+            return answer
+
+    def letterCombinations(self, digits: str) -> [str]:
+        mapping = {
+            2: ['a', 'b', 'c'],
+            3: ['d', 'e', 'f'],
+            4: ['g', 'h', 'i'],
+            5: ['j', 'k', 'l'],
+            6: ['m', 'n', 'o'],
+            7: ['p', 'q', 'r', 's'],
+            8: ['t', 'u', 'v'],
+            9: ['w', 'x', 'y', 'z']
+        }
+        answer = self.recursion(self, index=0, answer=[], word="", mapping=mapping, digits=digits)
+
+        return answer
+
+
+print(Solution.letterCombinations(Solution, ""))
